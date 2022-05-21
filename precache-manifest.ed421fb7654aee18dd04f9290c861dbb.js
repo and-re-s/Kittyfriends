cee24764788a8930e647e60ee1b0d7e00e310495
and-re-s <andre.syromyatnikov@gmail.com>
2022-05-21 14:38:28 +0600

    Updates

---
+++ precache-manifest.ed421fb7654aee18dd04f9290c861dbb.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "7bafaaf8fa946af53fb2",
+    "url": "/Kittyfriends/static/css/main.8c88d666.chunk.css"
+  },
+  {
+    "revision": "7bafaaf8fa946af53fb2",
+    "url": "/Kittyfriends/static/js/main.7bafaaf8.chunk.js"
+  },
+  {
+    "revision": "05a8e767b6b683ee5dc9",
+    "url": "/Kittyfriends/static/js/1.05a8e767.chunk.js"
+  },
+  {
+    "revision": "4ecd667bee4c39d94d44",
+    "url": "/Kittyfriends/static/css/2.6bec0ce6.chunk.css"
+  },
+  {
+    "revision": "4ecd667bee4c39d94d44",
+    "url": "/Kittyfriends/static/js/2.4ecd667b.chunk.js"
+  },
+  {
+    "revision": "180acf6390e758185e9c",
+    "url": "/Kittyfriends/static/js/runtime~main.180acf63.js"
+  },
+  {
+    "revision": "3944ba6fd5c9d92b775b0e1fc2b92aef",
+    "url": "/Kittyfriends/index.html"
+  }
+];
